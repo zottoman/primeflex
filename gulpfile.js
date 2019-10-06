16e4e19d8aa878b7d16137043a7998502c85920d
Christian Otto <christian.otto.1978@gmail.com>
2019-10-06 19:51:32 +0200

    feat: update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,35 @@
-var gulp = require('gulp'),
-    util = require("gulp-util"),
-    sass = require("gulp-sass"),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+"use strict";
 
-gulp.task("sass", function() {
-    // log("Generate CSS files " + (new Date()).toString());
-    gulp.src(sassFiles)
-        .pipe(sass({ style: 'expanded' }))
-                    .pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
-        .pipe(gulp.dest("dist"))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(gulp.dest('dist'));
+var autoprefixer = require('gulp-autoprefixer');
+var del = require('del');
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+var sassFiles = './src/**/*.scss';
+
+gulp.task('clean', function(done) {
+  del('dist').then(function() {
+    done();
+  });
 });
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('sass', function() {
+  return gulp.src(sassFiles)
+    .pipe(sourcemaps.init())
+    .pipe(sass({style: 'expanded'}))
+    .pipe(autoprefixer('last 3 version'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch(sassFiles, gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series('clean', 'sass', 'watch'));
